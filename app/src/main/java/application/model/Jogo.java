8045ad0e33af92731781b131f0cbd7466adcb796
Ellen Oliveira <oellen0306@gmail.com>
2024-09-06 00:00:23 +0000

    construindo as tabelas-modelo

---
+++ app/src/main/java/application/model/Jogo.java
@@ -0,0 +1,31 @@
+package application.model;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.processing.Generated;
+
+import jakarta.persitence.Column;
+import jakarta.persitence.Entity;
+import jakarta.persitence.GeneratedValue;
+import jakarta.persitence.GerantionType;
+import jakarta.persitence.Id;
+import jakarta.persitence.OneToMany;
+import jakarta.persitence.Table; 
+
+@Entity
+@Table (name = "jogos")
+public class Jogo{
+    @Id 
+    @GeneratedValue(strategy = GerantionType.IDENTIFY)
+    private long id;
+    @Column (unique = true, nullable = false)
+    private String titulo;
+
+    
+public String getTitulo() {
+    return titulo;
+}
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+}
+}
